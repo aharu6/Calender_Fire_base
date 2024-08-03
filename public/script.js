19681a3211d963aab43faab6b5674e5a766fcf71
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2024-08-03 22:20:23 +0900

    saveClickCount script design

--- public/script.js
+++ public/script.js
@@ -56,56 +56,9 @@ document.addEventListener("DOMContentLoaded", async () => {
   const calender = document.getElementById("calender");
   const click = document.getElementById("click");
   let clickedDates = JSON.parse(localStorage.getItem("clickedDates")) || {};
-  //generate calender
-  function generateCalender(year, month) {
-    calender.innerHTML = "";
-    const firstDay = new Date(year, month, 1);
-    const lastDay = new Date(year, month + 1, 0);
-
-    //first week day
-    const firstWeekDay = firstDay.getDay();
-    for (let i = 0; i < firstWeekDay; i++) {
-      const emptycell = document.createElement("th");
-      calender.appendChild(emptycell);
-    }
-
-    //date cell
-    //状態の復元
-    for (let date = Number(1); date <= lastDay.getDate(); date++) {
-      const cell = document.createElement("th");
-      calender.appendChild(cell);
-      cell.textContent = date;
-      cell.classList.add("cell");
-      cell.setAttribute("id", `${year}-${month + 1}-${date}`);
-      const dateKey = `${year}-${month + 1}-${date}`;
-      //クリック時の動作
-      cell.addEventListener("click", async (event) => {
-        const docRef = doc(db, "calender", dateKey);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          console.log("Document data:", docSnap.data().clicked);
-          await saveData(`${year}-${month + 1}-${date}`, docSnap.data().clicked);
-          updateCalender(
-            `${year}-${month + 1}-${date}`,
-            !docSnap.data().clicked
-          );
-          console.log("updatecalender");
-        } else {
-          awaitnewSaveData(`${year}-${month + 1}-${date}`, true);
-          updateCalender(`${year}-${month + 1}-${date}`, true);
-          console.log("true");
-        }
-        await countClick();
-      });
-    }
-  }
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = today.getMonth();
-  const date = today.getDate();
-  const dateKey = `${year}-${month + 1}-${date}`;
 
   generateCalender(today.getFullYear(), today.getMonth());
+  countClick();
 
   //dates には作成した日付の全てが入る「dateで合わせた日付でクリックしたデータを紐づけて表示する
   const lastDayRe = new Date(today.getFullYear(), today.getMonth() + 1, 0);
@@ -117,26 +70,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     console.log(clicked);
     updateCalender(date, clicked);
   }
-
-  //updatecalender
-  function updateCalender(date, clicked) {
-    let dateElement = document.getElementById(date);
-    if (dateElement) {
-      if (clicked === true) {
-        dateElement.classList.add("clicked");
-        console.log("updatecalenderclicked=true");
-      } else if (clicked === false) {
-        dateElement.classList.remove("clicked");
-        console.log("updatecalenderclicked = false");
-      } else {
-        console.log("not clicked");
-      }
-    }
-    console.log("updatecalenderfunction");
-  }
-
-  const head_month = document.getElementById("month");
-  head_month.textContent = today.getMonth() + 1;
 
   //prev month
   const prev = document.getElementById("preview");
@@ -165,98 +98,12 @@ document.addEventListener("DOMContentLoaded", async () => {
     loadData();
   });
 
-  //add user
-  //signUp
-  function signUp(email, password) {
-    const emailsakado = document.getElementById("signUpEmail").value;
-    const passWord = document.getElementById("signUpPassword").value;
-    createUserWithEmailAndPassword(auth, emailsakado, passWord)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        console.log("User refisterd:", user);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }
   window.signUp = signUp;
 
-  //signIn
-  function signIn(email, password) {
-    const email_sign = document.getElementById("signInEmail").value;
-    const password_sign = document.getElementById("signInPassword").value;
-    try {
-      signInWithEmailAndPassword(auth, email_sign, password_sign).then(
-        (userCredential) => {
-          const user = userCredential.user;
-          console.log("User logged in:", user);
-        }
-      );
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
   window.signIn = signIn;
 
-  //clickdata save　if docsnap.data().clickedが存在する時
-  async function saveData(date, clicked) {
-    try {
-      const user = auth.currentUser;
-      //ifdataexits なら!で反転させる
-      if (user) {
-        const docRef = doc(db, "calender", date);
-        const docSnap = await getDoc(docRef);
-        let newClickedData = !clicked;
-        const data = docSnap.data();
-        console.log(newClickedData);
-        await setDoc(docRef, { clicked: newClickedData, userId: user.uid });
-        console.log("Document written with ID;", docRef.id, newClickedData);
-      } else {
-        console.error("No user is signed in");
-      }
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  }
-  //docsnap.data().clickedが存在しない時のsaveData関数
-  async function newSaveData(date, clicked) {
-    try {
-      const user = auth.currentUser;
-      if (user) {
-        const docRef = doc(db, "calender", date);
-        const docSnap = await getDoc(docRef);
-        let newClickedData = clicked;
-        await setDoc(docRef, { clicked: newClickedData, userId: user.uid });
-      } else {
-        console.error("No user is signed in");
-      }
-    } catch (e) {
-      console.error("Error adding document:", e);
-    }
-  }
   document.querySelectorAll;
 
-  //load data
-  async function loadData() {
-    try {
-      const user = auth.currentUser;
-      console.log(user.uid);
-      if (user) {
-        const querySnapshot = await getDocs(collection(db, "calender"));
-        querySnapshot.forEach((doc) => {
-          const data = doc.data();
-          if (data.userId === user.uid) {
-            console.log(`${doc.id} => ${doc.data().clicked}`);
-            updateCalender(doc.id, doc.data().clicked);
-          }
-        });
-      } else {
-        console.error("No user is signed in");
-      }
-    } catch (e) {
-      console.error("Error adding document:", e);
-    }
-  }
   window.onload = loadData;
 
   //observe user
@@ -278,37 +125,193 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
   });
 
-  //retrieve click data
-  async function getClickdata(date) {
-    const docRef = doc(db, "clickData", date);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      return docSnap.data().clicked;
-    } else {
-      console.log("No such document!");
-      return null;
-    }
-  }
-
   //logout
   const logout = document.getElementById("logout");
   logout.addEventListener("click", () => {
     signOut(auth);
   });
-  function signOut(auth) {
-    auth.signOut().then(() => {
-      console.log("User is signed out");
-      generateCalender(today.getFullYear(), today.getMonth());
-      const dropdownMenuButton = document.getElementById("dropdownMenuButton");
-      dropdownMenuButton.textContent = "NoUser";
-      //signIn review
-      document.getElementById("signInEmail").style.display = "block";
-      document.getElementById("signInPassword").style.display = "block";
-      document.getElementById("signIn").style.display = "block";
-      document.getElementById("logout").style.display = "none";
+});
+//generate calender
+function generateCalender(year, month) {
+  calender.innerHTML = "";
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, month + 1, 0);
+
+  //first week day
+  const firstWeekDay = firstDay.getDay();
+  for (let i = 0; i < firstWeekDay; i++) {
+    const emptycell = document.createElement("th");
+    calender.appendChild(emptycell);
+  }
+
+  //date cell
+  //状態の復元
+  for (let date = Number(1); date <= lastDay.getDate(); date++) {
+    const cell = document.createElement("th");
+    calender.appendChild(cell);
+    cell.textContent = date;
+    cell.classList.add("cell");
+    cell.setAttribute("id", `${year}-${month + 1}-${date}`);
+    const dateKey = `${year}-${month + 1}-${date}`;
+    //クリック時の動作
+    cell.addEventListener("click", async (event) => {
+      const docRef = doc(db, "calender", dateKey);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data().clicked);
+        await saveData(`${year}-${month + 1}-${date}`, docSnap.data().clicked);
+        updateCalender(`${year}-${month + 1}-${date}`, !docSnap.data().clicked);
+        console.log("updatecalender");
+      } else {
+        await newSaveData(`${year}-${month + 1}-${date}`, true);
+        updateCalender(`${year}-${month + 1}-${date}`, true);
+        console.log("newSaveData");
+      }
+      await countClick();
     });
   }
-});
+}
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth();
+const date = today.getDate();
+const dateKey = `${year}-${month + 1}-${date}`;
+
+//updatecalender
+function updateCalender(date, clicked) {
+  let dateElement = document.getElementById(date);
+  if (dateElement) {
+    if (clicked === true) {
+      dateElement.classList.add("clicked");
+      console.log("updatecalenderclicked=true");
+    } else if (clicked === false) {
+      dateElement.classList.remove("clicked");
+      console.log("updatecalenderclicked = false");
+    } else {
+      console.log("not clicked");
+    }
+  }
+  console.log("updatecalenderfunction");
+}
+
+const head_month = document.getElementById("month");
+head_month.textContent = today.getMonth() + 1;
+//add user
+//signUp
+function signUp(email, password) {
+  const emailsakado = document.getElementById("signUpEmail").value;
+  const passWord = document.getElementById("signUpPassword").value;
+  createUserWithEmailAndPassword(auth, emailsakado, passWord)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("User refisterd:", user);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+//signIn
+function signIn(email, password) {
+  const email_sign = document.getElementById("signInEmail").value;
+  const password_sign = document.getElementById("signInPassword").value;
+  try {
+    signInWithEmailAndPassword(auth, email_sign, password_sign).then(
+      (userCredential) => {
+        const user = userCredential.user;
+        console.log("User logged in:", user);
+      }
+    );
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+//clickdata save　if docsnap.data().clickedが存在する時
+async function saveData(date, clicked) {
+  try {
+    const user = auth.currentUser;
+    //ifdataexits なら!で反転させる
+    if (user) {
+      const docRef = doc(db, "calender", date);
+      const docSnap = await getDoc(docRef);
+      let newClickedData = !clicked;
+      const data = docSnap.data();
+      console.log(newClickedData);
+      await setDoc(docRef, { clicked: newClickedData, userId: user.uid });
+      console.log("Document written with ID;", docRef.id, newClickedData);
+    } else {
+      console.error("No user is signed in");
+    }
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+//docsnap.data().clickedが存在しない時のsaveData関数
+async function newSaveData(date, clicked) {
+  try {
+    const user = auth.currentUser;
+    if (user) {
+      const docRef = doc(db, "calender", date);
+      const docSnap = await getDoc(docRef);
+      let newClickedData = clicked;
+      await setDoc(docRef, { clicked: newClickedData, userId: user.uid });
+    } else {
+      console.error("No user is signed in");
+    }
+  } catch (e) {
+    console.error("Error adding document:", e);
+  }
+}
+
+//load data
+async function loadData() {
+  try {
+    const user = auth.currentUser;
+    console.log(user.uid);
+    if (user) {
+      const querySnapshot = await getDocs(collection(db, "calender"));
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        if (data.userId === user.uid) {
+          console.log(`${doc.id} => ${doc.data().clicked}`);
+          updateCalender(doc.id, doc.data().clicked);
+        }
+      });
+    } else {
+      console.error("No user is signed in");
+    }
+  } catch (e) {
+    console.error("Error adding document:", e);
+  }
+}
+
+//retrieve click data
+async function getClickdata(date) {
+  const docRef = doc(db, "clickData", date);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    return docSnap.data().clicked;
+  } else {
+    console.log("No such document!");
+    return null;
+  }
+}
+
+function signOut(auth) {
+  auth.signOut().then(() => {
+    console.log("User is signed out");
+    generateCalender(today.getFullYear(), today.getMonth());
+    const dropdownMenuButton = document.getElementById("dropdownMenuButton");
+    dropdownMenuButton.textContent = "NoUser";
+    //signIn review
+    document.getElementById("signInEmail").style.display = "block";
+    document.getElementById("signInPassword").style.display = "block";
+    document.getElementById("signIn").style.display = "block";
+    document.getElementById("logout").style.display = "none";
+  });
+}
 //click day count
 
 async function countClick() {
